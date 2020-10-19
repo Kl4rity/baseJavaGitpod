a11a2434dfe3067a339f9a78b418d781c39c2fdd
Clemens Stift <clemens@stift.online>
2020-10-19 15:31:20 +0000

    Hello world commit.

---
+++ src/test/java/org/example/HelloWorldApplication/HelloWorldApplicationTests.java
@@ -0,0 +1,14 @@
+package org.example.HelloWorldApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+        System.out.println("Works like a charm!");
+	}
+
+}
