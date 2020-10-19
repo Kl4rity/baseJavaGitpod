7886391ed15126cad338e505db8317c820556b10
Clemens Stift <clemens@stift.online>
2020-10-19 17:28:50 +0000

    Work in Progress.

--- src/main/java/org/example/HelloWorldApplication/studentsTask/StudentsWhichAreIll.java
+++ src/main/java/org/example/HelloWorldApplication/studentsTask/StudentsWhichAreIll.java
@@ -7,6 +7,11 @@ import org.example.HelloWorldApplication.studentsTask.people.*;
 
 public class StudentsWhichAreIll {
     public static List<Person> findOverlapBetweenStudentsAndPersons(List<Student> students, List<Person> illPeople) {
-        return List.of();
+        return students.stream().filter(it -> {
+            illPeople.stream().map(
+                shadowIt -> {
+                    return (it.firstName == shadowIt.firstName && it.lastName == shadowIt.lastName);
+            }).anyMatch(matchedWithOtherList -> matchedWithOtherList);
+        }).collect(Collectors.toList());
     }
 }
