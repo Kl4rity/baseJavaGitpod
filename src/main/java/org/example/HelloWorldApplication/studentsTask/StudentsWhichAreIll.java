ed5a4be5ec8a8bb12248d2946028aa2ba33fbfb4
Clemens Stift <clemens@stift.online>
2020-10-20 06:31:25 +0000

    Simplify for better usability.

--- src/main/java/org/example/HelloWorldApplication/studentsTask/StudentsWhichAreIll.java
+++ src/main/java/org/example/HelloWorldApplication/studentsTask/StudentsWhichAreIll.java
@@ -6,7 +6,16 @@ import java.util.stream.Collectors;
 import org.example.HelloWorldApplication.studentsTask.people.*;
 
 public class StudentsWhichAreIll {
-    public static List<Person> findOverlapBetweenStudentsAndPersons(List<Student> students, List<Person> illPeople) {
+
+    public List<Student> students;
+    public List<Person> illPeople;
+
+    public StudentsWhichAreIll() {
+        students = getListOfStudents();
+        illPeople = getListOfIllPeople();
+    }
+
+    public List<Person> findOverlapBetweenStudentsAndPeople() {
         return students.stream().filter(it -> {
             return illPeople.stream()
             .map(shadowIt -> peopleDoMatch(it, shadowIt))
@@ -14,9 +23,28 @@ public class StudentsWhichAreIll {
         }).collect(Collectors.toList());
     }
 
-    public static Boolean peopleDoMatch(Person person, Person personToCompareTo) {
+    public Boolean peopleDoMatch(Person person, Person personToCompareTo) {
         Boolean firstNameMatches = person.firstName == personToCompareTo.firstName;
         Boolean lastNameMatches = person.lastName == personToCompareTo.lastName;
         return firstNameMatches && lastNameMatches;
+    }
+
+    private List<Student> getListOfStudents() {
+        return List.of(
+            new Student("Hans", "Müller"),
+            new Student("John","Doe"),
+            new Student("Max","Mustermann"),
+            new Student("Fritz", "Schmidt")
+        );
+    }
+
+    private List<Person> getListOfIllPeople(){
+        return List.of(
+            new Person("Karl","Meier"),
+            new Person("Fritz","Schmidt"),
+            new Person("Max","Doe"),
+            new Person("Hans","Müller"),
+            new Person("Sebastian","Lee")
+        );
     }
 }
--- src/test/java/org/example/HelloWorldApplication/studentsTask/StudentsWhichAreIllUnitTest.java
+++ src/test/java/org/example/HelloWorldApplication/studentsTask/StudentsWhichAreIllUnitTest.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.example.HelloWorldApplication.studentsTask.people.Person;
 import org.example.HelloWorldApplication.studentsTask.people.Student;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.slf4j.LoggerFactory;
@@ -13,55 +12,28 @@ import org.slf4j.Logger;
 public class StudentsWhichAreIllUnitTest {
 
     private Logger logger;
-    public List<Student> students;
-    public List<Person> illPeople;
 
     public StudentsWhichAreIllUnitTest(){
         logger = LoggerFactory.getLogger(this.getClass());
     }
 
-    @BeforeEach
-    void setup() {
-        students = getListOfStudents();
-        illPeople = getListOfIllPeople();
-    }
-
     @Test
     void GIVEN_two_lists_with_overlap_WHEN_findOverlap_is_called_THEN_collection_containing_overlap_is_returned() {
-        List<Person> overlap = StudentsWhichAreIll.findOverlapBetweenStudentsAndPersons(students, illPeople);
+        List<Person> overlap = new StudentsWhichAreIll().findOverlapBetweenStudentsAndPeople();
         logger.info(overlap.toString());
         assert(overlap.size() == 2);
     }
 
     @Test
     void GIVEN_two_lists_with_overlap_WHEN_findOverlap_is_called_THEN_resulting_collection_contains_Fritz_Schmidt(){
-        List<Person> overlap = StudentsWhichAreIll.findOverlapBetweenStudentsAndPersons(students, illPeople);
+        List<Person> overlap = new StudentsWhichAreIll().findOverlapBetweenStudentsAndPeople();
         assert(overlap.stream().anyMatch(it -> it.firstName == "Fritz" && it.lastName == "Schmidt"));
     }
 
     @Test
     @Disabled
     void GIVEN_two_lists_with_overlap_WHEN_findOverlap_is_called_THEN_resulting_collection_contains_Hans_Müller(){
-        List<Person> overlap = StudentsWhichAreIll.findOverlapBetweenStudentsAndPersons(students, illPeople);
+        List<Person> overlap = new StudentsWhichAreIll().findOverlapBetweenStudentsAndPeople();
         assert(overlap.contains(new Student("Fritz", "Schmidt")));
-    }
-
-    private List<Student> getListOfStudents() {
-        return List.of(
-            new Student("Hans", "Müller"),
-            new Student("John","Doe"),
-            new Student("Max","Mustermann"),
-            new Student("Fritz", "Schmidt")
-        );
-    }
-
-    private List<Person> getListOfIllPeople(){
-        return List.of(
-            new Person("Karl","Meier"),
-            new Person("Fritz","Schmidt"),
-            new Person("Max","Doe"),
-            new Person("Hans","Müller"),
-            new Person("Sebastian","Lee")
-        );
     }
 }
